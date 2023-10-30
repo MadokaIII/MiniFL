df39cfc025548ffa022205fd7bfa6f6a0f8f7f07
daoudlamalmi <daoudlamalmi@gmail.com>
2023-10-30 04:07:26 +0100

    Initial commit

---
+++ src/Lib.hs
@@ -0,0 +1,24 @@
+The lambdas must flow.
+If I break, you can:
+  1. Restart:           M-x haskell-process-restart
+  2. Configure logging: C-h v haskell-process-log (useful for debugging)
+  3. General config:    M-x customize-mode
+  4. Hide these tips:   C-h v haskell-process-show-debug-tips
+λ> 1
+
+<interactive>:7:1: warning: [-Wtype-defaults]
+    • Defaulting the type variable ‘a0’ to type ‘Integer’ in the following constraints
+        (Show a0) arising from a use of ‘print’ at <interactive>:7:1
+        (Num a0) arising from a use of ‘it’ at <interactive>:7:1
+    • In a stmt of an interactive GHCi command: print it
+1
+The next big Haskell project is about to start!
+If I break, you can:
+  1. Restart:           M-x haskell-process-restart
+  2. Configure logging: C-h v haskell-process-log (useful for debugging)
+  3. General config:    M-x customize-mode
+  4. Hide these tips:   C-h v haskell-process-show-debug-tips
+λ> :quit
+:q
+Leaving GHCi.
+λ> nnn:wlllsnnn
